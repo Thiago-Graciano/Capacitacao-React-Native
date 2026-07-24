7bf128afd5be567f84da7e74f7759193d3b1930d
Thiago Graciano <msgraciano28@gmail.com>
2026-07-24 11:15:45 -0300

    Feat: Implementa funcao de signOut

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import { AuthContext } from '../../contexts/auth';
 
 export default function Home() {
+    const { signOut, user } = useContext(AuthContext);
+
     return (
         <View>
             <Text>Home</Text>
+            <Text>Nome: {user.name} </Text>
+
+            <Button
+                title="Sair da Conta"
+                onPress={() => signOut()}
+            />
+
         </View>
     )
 }
