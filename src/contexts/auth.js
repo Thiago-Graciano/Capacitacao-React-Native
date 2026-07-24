3840fca40c5e1fda6446acea43216556f3bc4960
Thiago Graciano <msgraciano28@gmail.com>
2026-07-23 22:54:59 -0300

    Feat: Implementa UX de loading e login de usuario

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -5,16 +5,16 @@ import { useNavigation } from '@react-navigation/native'
 
 export const AuthContext = createContext({})
 
-function AuthProvider({ children }){
+function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loadingAuth, setLoadingAuth] = useState(false)
 
     const navigation = useNavigation();
 
-    async function signUp(nome, email, password){
+    async function signUp(nome, email, password) {
         setLoadingAuth(true)
-        
-        try{
+
+        try {
             const response = await api.post('/users', {
                 name: nome,
                 password: password,
@@ -24,17 +24,53 @@ function AuthProvider({ children }){
             setLoadingAuth(false)
             navigation.goBack();
 
-        }catch(err){
+        } catch (err) {
             console.log("Erro ao cadastrar")
             setLoadingAuth(false)
         }
     }
 
-    return(
-        <AuthContext.Provider value={{ signed: !!user, user, signUp, loadingAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
+    async function signIn(email, password) {
+        setLoadingAuth(true);
+
+        try {
+            const response = await api.post('/login', {
+                email: email,
+                password: password,
+            })
+
+            const { id, name, token } = response.data;
+
+            const data = {
+                id,
+                name,
+                token,
+                email,
+            }
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+            setUser({
+                id,
+                name,
+                email,
+            });
+
+            setLoadingAuth(false);
+
+        } catch (err) {
+            console.log("Erro ao logar ", err);
+            //setLoadingAuth(false);
+        }
+    }
+
+    
+
+return (
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loadingAuth }}>
+        {children}
+    </AuthContext.Provider>
+)
 }
 
 export default AuthProvider;
