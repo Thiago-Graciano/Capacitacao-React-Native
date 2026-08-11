4419e4810c0c026a3bff8310e025fa45ba525507
Thiago Graciano <msgraciano28@gmail.com>
2026-08-11 10:45:57 -0300

    Feat: Adiciona modal para filtro de datas nas movimentações

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { TouchableOpacity, Alert } from 'react-native';
+import { TouchableOpacity, Alert, Modal } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth';
 
@@ -18,6 +18,7 @@ import { format } from 'date-fns';
 import { useIsFocused } from '@react-navigation/native';
 import BalanceItem from '../../components/BalanceItem';
 import HistoricoList from '../../components/HistoricoList';
+import CalendarModal from '../../components/CalendarModal';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -25,6 +26,7 @@ export default function Home() {
     const isFocused = useIsFocused();
     const [listBalance, setListBalance] = useState([]);
     const [movements, setMovements] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
 
     const [dateMovements, setDateMovements] = useState(new Date());
 
@@ -37,7 +39,7 @@ export default function Home() {
 
             const receives = await api.get('/receives', {
                 params: {
-                    date:dateFormated
+                    date: dateFormated
                 }
             })
 
@@ -60,7 +62,7 @@ export default function Home() {
     }, [isFocused, dateMovements]);
 
     async function handleDelete(id) {
-        try{
+        try {
             await api.delete('/receives/delete', {
                 params: {
                     item_id: id
@@ -86,19 +88,32 @@ export default function Home() {
             />
 
             <Area>
-                <TouchableOpacity style={{ marginBottom: 15 }}>
-                    <Icon name="event" color="#121212" size={30}  />
+                <TouchableOpacity
+                    onPress={ () => setModalVisible(true) }
+                    style={{ marginBottom: 15 }}
+                >
+                    <Icon name="event" color="#121212" size={30} />
                 </TouchableOpacity>
                 <Title>Últimas Movimentações</Title>
             </Area>
 
             <List
                 data={movements}
-                keyExtractor={ item => item.id }
-                renderItem={ ({item}) => <HistoricoList data={item} deleteItem={handleDelete} /> }
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <HistoricoList data={item} deleteItem={handleDelete} />}
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingBottom: 20 }}
             />
+
+            <Modal
+                visible={modalVisible}
+                animationType="fade"
+                transparent={true}
+            >
+                <CalendarModal 
+                    setVisible={ () => setModalVisible(false) }
+                />
+            </Modal>
 
         </Background>
     )
